f286c9f1c76b1fb274744db06d3dfe4229b65dfb
Thobias <thobiasl@hiof.no>
2025-09-17 09:11:21 +0200

    testing av data overføring via json

--- core/main.py
+++ core/main.py
@@ -3,8 +3,9 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) # Add the parent directory to the Python path
 from adapters.BME280_adapter import BME280Sensor, average
 from adapters.ledstrip_adapter import LEDStripAdapter
-from application.gui_data_bridge import Readings, update_readings
+#from application.gui_data_bridge import Readings, update_readings
 import time
+
 
 def main():
     # Seting up variables
@@ -71,7 +72,10 @@ def main():
 
             # Skriv ut verdiene
             print("sensors online")
-            update_readings(Readings(
+            def data_list():
+                return {"temperature1": temperature1, "temperature2": temperature2, "average_temp": average_temp,
+                        "humidity1": humidity1, "humidity2": humidity2, "average_humidity": average_humidity}
+            '''update_readings(Readings(
                 ts=time.time(),
                 temperature1=temperature1,
                 temperature2=temperature2,
@@ -79,8 +83,9 @@ def main():
                 humidity1=humidity1,
                 humidity2=humidity2,
                 average_humidity=average_humidity,
-            ))
+            ))'''
             #guiReadingData_adapter(temperature1, temperature2, average_temp, humidity1, humidity2, average_humidity)
+
 
             time.sleep(2)
         except KeyboardInterrupt:
